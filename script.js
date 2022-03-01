c34de0aff5dca7a97afb5592c8cf440087e048f4
JonoHasMono <fry22331@lcsd2kids.org>
2022-03-01 09:37:17 -0700

    push from pc to laptop

--- script.js
+++ script.js
@@ -37,13 +37,32 @@ let adown = false;
 let sdown = false;
 let ddown = false;
 
+//! All voice sound effects
+let voiceJono = new Audio('sounds/voice_jono.mp3')
+let voiceRonald = new Audio('sounds/voice_ronald.mp3')
+
 //! All messages from the first scene
-let sceneCharIcon = document.createElement('div');
 let sceneMsg1 = "";
 let sceneMsg2 = "";
+let sceneMsg3 = "";
+let sceneMsg4 = "";
+let sceneMsg5 = "";
+let sceneMsg6 = "";
+let sceneMsg7 = "";
+let sceneMsg8 = "";
 let messageCreated = false;
+let messageIcon = document.createElement('img');
+let messageIconChar = '';
+let messageIconEnemy = '';
+messageIcon.classList.add('messageIcon');
 let s1m1j = "Ayo, I think you took a wrong turn at the Anime Convention, homie."
-let s1m2j = "Are you insulting my costume? Not very poggers."
+let s1m2j = "Are you insulting my costume, beta male?"
+let s1m3j = "That wasn't very poggers of you."
+let s1m4j = "..."
+let s1m5j = "That was, without a doubt,"
+let s1m6j = "The dumbest thing that I have ever heard."
+let s1m7j = "You shut your mouth about my costume, virgin!"
+let s1m8j = "Me and my friends ought to teach you a lesson!"
 
 function startGame() {
     if(startButtonClicked == false) {
@@ -106,8 +125,18 @@ function startGame() {
 
         function startSceneOne() {
             if(chosenCharacter == 'jono') {
-                sceneMsg1 = s1m1j;
+                sceneMsg1 = s1m1j
                 sceneMsg2 = s1m2j;
+                sceneMsg3 = s1m3j;
+                sceneMsg4 = s1m4j;
+                sceneMsg5 = s1m5j;
+                sceneMsg6 = s1m6j;
+                sceneMsg7 = s1m7j;
+                sceneMsg8 = s1m8j;
+                messageIconChar1 = "images/" + chosenCharacter + "_exp_1.png"
+                messageIconChar2 = "images/" + chosenCharacter + "_exp_2.png"
+                messageIconChar3 = "images/" + chosenCharacter + "_exp_3.png"
+                messageIconEnemy = 'images/ronald_exp_1.png'
             }
             let messageBG = document.createElement('div');
             messageBG.classList.add('messageBG');
@@ -122,37 +151,99 @@ function startGame() {
                     nextMessage.addEventListener('click', function() {
                         createNextMessage();
                     });
-                    bodyvar.appendChild(nextMessage);
 
-            
+                    messageIcon.setAttribute('src', messageIconChar1);
+                    messageIcon.classList.add('messageIconAnim')
+                    bodyvar.appendChild(nextMessage);
+                    bodyvar.appendChild(messageIcon);
 
                     function createNextMessage() {
                         currentMsgValue++
                         if(currentMsgValue == 2) {
-                            createMessage(sceneMsg2);
-                            console.log('message stuff')
+                            messageIcon.removeAttribute('src', messageIconChar1);
+                            messageIcon.classList.remove('messageIconAnim')
+                            messageIcon.setAttribute('src', messageIconEnemy);
+                            setTimeout(() => {
+                                messageIcon.classList.add('messageIconAnim')
+                            }, 1);
+                            createMessage(sceneMsg2, 35, voiceRonald);
+                        } else if(currentMsgValue == 3) {
+                            createMessage(sceneMsg3, 80, voiceRonald);
+                        } else if(currentMsgValue == 4) {
+                            messageIcon.removeAttribute('src', messageIconEnemy);
+                            messageIcon.classList.remove('messageIconAnim')
+                            messageIcon.setAttribute('src', messageIconChar2);
+                            setTimeout(() => {
+                                messageIcon.classList.add('messageIconAnim')
+                            }, 1);
+                            createMessage(sceneMsg4, 500, voiceJono);
+                        } else if(currentMsgValue == 5) {
+                            messageIcon.removeAttribute('src', messageIconChar2);
+                            messageIcon.setAttribute('src', messageIconChar1);
+                            createMessage(sceneMsg5, 6, voiceJono);
+                        } else if(currentMsgValue == 6) {
+                            messageIcon.removeAttribute('src', messageIconChar1);
+                            messageIcon.setAttribute('src', messageIconChar3);
+                            createMessage(sceneMsg6, 40, voiceJono);
+                        } else if(currentMsgValue == 7) {
+                            messageIcon.removeAttribute('src', messageIconChar3);
+                            messageIcon.classList.remove('messageIconAnim')
+                            messageIcon.setAttribute('src', messageIconEnemy);
+                            setTimeout(() => {
+                                messageIcon.classList.add('messageIconAnim')
+                            }, 1);
+                            createMessage(sceneMsg7, 35, voiceRonald);
+                        } else if(currentMsgValue == 8) {
+                            createMessage(sceneMsg8, 45, voiceRonald);
                         }
+                        
                     }
 
             let currentMsgValue = 1;
-            setTimeout(() => {
-                createMessage(sceneMsg1);
+            setTimeout(() => { 
+                createMessage(sceneMsg1, 35, voiceJono);
             }, 200);
-                function createMessage(x) {
+                function createMessage(x,y,z) {
                     let i = 0;
                     let text = x;
-                    let speed = 35;
+                    let speed = y;
+                    let voice = z;
+                    let voiceReady = true;
                     console.log(x);
                     textMessage.innerHTML = "";
                     typewriter();
+                    playVoice();
         
                     function typewriter() {
-                        if (i < text.length) {
+                        if (i <= text.length) {
                           textMessage.innerHTML += text.charAt(i);
                           i++;
                           setTimeout(typewriter, speed);
                         }
                       }
+
+                    function playVoice() {
+                        if (voiceReady == true) {
+                            voiceReady = false;
+                            if(speed > 100) {
+                                setTimeout(() => {
+                                    voiceReady = true;
+                                }, speed);
+                            } else{
+                                setTimeout(() => {
+                                    voiceReady = true;
+                                }, 100);
+                            }
+                        if (i <= text.length) {
+                            let originalVoice = voice;
+                            let newVoice = originalVoice.cloneNode();
+                            newVoice.volume = 0.5;
+                            newVoice.play();
+    
+                        }
+                    }
+                        setTimeout(playVoice, 5);
+                    }
                 }
 
                 
